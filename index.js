d84bc0924e5b1231accf9316b66eaa9b091b0a9c
afairlie <ariane.fairlie@gmail.com>
2020-04-10 02:49:49 +0000

    added call to fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
+  } else {
+    fetchCoordsByIP(ip, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      } else {
+        console.log(data);
+      }
+    });
   }
-
-  console.log('It worked! Returned IP:' , ip);
 });
+
