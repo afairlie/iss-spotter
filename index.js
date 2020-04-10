889ed76e001c968b729bb0b45da0e14e6ea048b8
afairlie <ariane.fairlie@gmail.com>
2020-04-10 03:35:53 +0000

    added formatting function for logging passes

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
-nextISSTimesForMyLocation((error, passTimes) => {
+const printPassTimes = (passTimes) => {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, flyover) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
-  console.log(passTimes);
+  printPassTimes(flyover);
 });
 
